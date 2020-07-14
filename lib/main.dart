3754c29db6790a9999ae2f7bee7d1afe8cd55995
Paulo Cesar Rodrigues <rodrigpc@ig.com.br>
2020-07-13 22:59:29 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+  
+}
